cbb930c4009398fc06aa6dd9272e5eb892a3d66a
tomheller <office@thomasheller.net>
2017-05-05 21:38:11 +0200

    add a simple goal class

---
+++ js/goal.js
@@ -0,0 +1,15 @@
+class Goal {
+  constructor(x = 20, y = 20, rad = 20) {
+    this.x = x;
+    this.y = y; 
+    this.rad = rad; 
+    this.matter = Matter.Bodies.circle(this.x, this.y, this.rad, {
+      isStatic: true,
+      render: {
+        fillStyle: '#34bb44',
+        strokeStyle: '#34aabb',
+      }
+    });
+  }
+
+}
